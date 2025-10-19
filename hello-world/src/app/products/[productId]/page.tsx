3251ef896bddbc8ed54c4763ffbf66b72652962a
sory521 <sorybangs94@hotmail.com>
2025-10-19 14:40:31 +0200

    6- dynamic routes

---
+++ hello-world/src/app/products/[productId]/page.tsx
@@ -0,0 +1,6 @@
+
+
+export default async function ProductDetails({params}: Readonly<{params: { productId: string }}>) {
+  const { productId } = params;
+  return <h1>Details about product {productId}</h1>;
+}
