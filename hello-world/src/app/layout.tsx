29b05555a4bee6e9bfbe7dce1fb12cbf3e172a5c
sory521 <sorybangs94@hotmail.com>
2025-10-19 20:34:20 +0200

    11- Introduction aux layouts

--- hello-world/src/app/layout.tsx
+++ hello-world/src/app/layout.tsx
@@ -27,7 +27,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem"
+          }}
+        >
+          <p>header</p>
+        </header>
         {children}
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem"
+          }}  
+        >
+          Footer
+        </footer>
       </body>
     </html>
   );
