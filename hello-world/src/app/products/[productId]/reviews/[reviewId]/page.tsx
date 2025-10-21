f95182c648e2120380738edd887ac3cf9a3d2495
sory521 <sorybangs94@hotmail.com>
2025-10-21 19:18:59 +0200

    22- Handling error avec error.tsx

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,9 @@
 
 import { redirect } from "next/navigation";
+
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
 export default async function ProductReview({ params }:
     Readonly<{
         params: {
@@ -7,6 +11,10 @@ export default async function ProductReview({ params }:
             reviewId: string;
         }
     }>) {
+    const random = getRandomInt(2);
+    if (random === 1) {
+        throw new Error("Error Loading review")
+    }
     const { productId, reviewId } = params;
     if (Number.parseInt(reviewId) > 1000) {
         // notFound();
