31c4ff1314b621b2ff03969492d14b2dfde8eeaa
sory521 <sorybangs94@hotmail.com>
2025-10-21 19:06:38 +0200

    21 -  Loading file

--- hello-world/src/app/blog/page.tsx
+++ hello-world/src/app/blog/page.tsx
@@ -1,3 +1,8 @@
-export default function Blog() {
+export default async function Blog() {
+    await new Promise(resole => {
+        setTimeout(() => {
+            resole("international delay")
+        }, 2000);
+    });
     return <h1>My blog</h1>
 }
